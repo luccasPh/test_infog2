77b3798aaa7cf462bb35db63487b70de033745d3
Lucas Pinheiro <lucas4pinheiro@gmail.com>
2021-07-10 09:56:09 -0300

    Changed survivor urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path("", views.CreateSurvivorView.as_view()),
-    path("<int:pk>/location", views.UpdateSurvivorLocationView.as_view()),
-    path("<int:pk>/infected", views.UpdateSurvivorInfectedView.as_view()),
+    path("survivors/", views.CreateSurvivorView.as_view()),
+    path("survivors/<int:pk>/location", views.UpdateSurvivorLocationView.as_view()),
+    path("survivors/<int:pk>/infected", views.UpdateSurvivorInfectedView.as_view()),
 ]
--- app/app/urls.py
+++ app/app/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/survivors/", include("api.urls")),
+    path("api/", include("api.urls")),
 ]
